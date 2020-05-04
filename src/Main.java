64fa0818ef6baab0d2e74a85d9094d78cb7df14e
saikat111 <s8856010@gmail.com>
2020-05-04 00:21:05 -0400

    4/4/20

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]){
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+        System.out.println(stack.pop());
+        stack.printStack();
+
+       // System.out.println(stack.top);
+    }
+}
+
